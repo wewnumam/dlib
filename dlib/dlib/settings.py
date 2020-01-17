7cc6642df07a920e728ddba536df0035d1042990
Ahmad Nuhisya <wvwnumam@gmail.com>
2020-01-17 09:43:09 +0700

    Memperbaiki static file

--- dlib/dlib/settings.py
+++ dlib/dlib/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILE_DIRS = [
+STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
