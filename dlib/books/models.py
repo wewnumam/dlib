ea927fe67ab9cb483a5527ccac1623df5cb13028
Ahmad Nuhisya <wvwnumam@gmail.com>
2020-01-22 12:58:50 +0700

    Menambahkan filefield pada models

--- dlib/books/models.py
+++ dlib/books/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=64)
     category = models.CharField(max_length=64)
     writer = models.CharField(max_length=64)
     synopsis = models.TextField()
+    cover = models.FileField(upload_to='static/uploads/', max_length=128, default='default.jpg')
+    filename = models.FileField(upload_to='static/uploads/', max_length=128, default='default.pdf')
 
     def __str__(self):
         return "{}".format(self.title)
--- dlib/books/views.py
+++ dlib/books/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render 
 
+from .models import Book
+
 def index(request):
+    books = Book.objects.all()
     context = {
         'title': 'dlib | Digital Library',
-        'heading': 'Read It!'
+        'heading': 'Read It!',
+        'books': books,
     }
     return render(request, 'books/index.html', context)
